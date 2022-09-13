93284f15def41b97774107046076499e124afbc2
Ashok Amireddy <ashokamireddy@Ashoks-MacBook-Pro.local>
2022-09-12 19:04:42 -0600

    Revert "added heroku postgressql as database instead of sqlite3"
    This reverts commit 9426576110ce786a166f1debc4260e715be76b6a.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -10,10 +9,13 @@ from resources.store import Store, StoreList
 from SQLAlchemydb import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.secret_key = "queen"
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 jwt = JWT(app, authenticate, identity) #/auth
 
@@ -24,5 +26,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == "__main__":
+    db.init_app(app)
     app.run(debug=True, port=5001)
 
