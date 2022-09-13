85a8b2f80a5ae568071fcac62b0f29bd0287ea91
Ashok Amireddy <ashokamireddy@Ashoks-MacBook-Pro.local>
2022-09-13 11:40:06 -0600

    added uri variable

--- app.py
+++ app.py
@@ -11,7 +11,11 @@ from SQLAlchemydb import db
 
 app = Flask(__name__)
 app.secret_key = "queen"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = os.getenv('DATABASE_URL')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 api = Api(app)
 
 
